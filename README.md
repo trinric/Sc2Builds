844143c9b809dc26431ceaeb177854daea524628
Ryan Wilson <trinric@gmail.com>
2015-03-19 21:50:04 -0400

    Got basic filtering for each race fixed. Very messy

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 Many of the existing Sc2 build order sites are not updated frequently enough and don't have high enough standards of builds. This site will show the most recent and highly liked builds by the community.
 
+
+## How to test locally
+
+You can clone this
+```
+git clone https://github.com/trinric/Sc2Builds.git
+```
+Make sure you have the latest version of meteor installed.
+```
+cd Sc2Builds
+meteor
+```
--- client/builds/templates/builds_list.js
+++ client/builds/templates/builds_list.js
@@ -1,8 +1,27 @@
+/**
+* This is the basic filtering for the table on the front page
+* Going to clean this up later since there is a lot of almost repeated lines
+*
+*/
+var racelist = ['zerg', 'terran', 'protoss'];
+
 Template.buildsList.helpers({
-	settings : function() {
-		console.log("here");
-		return {
-			fields : ['name' , 'race', 'against']
+	builds : function() {
+		if (typeof(Session.get('race-select')) == 'undefined' || Session.get('race-select') == 'all races'){
+			Session.set('race-select', racelist);
 		}
+		if (typeof(Session.get('against-select')) == 'undefined' || Session.get('against-select') == 'all races'){
+			Session.set('against-select', racelist);
+		}
+		return Builds.find({race : {$in : Session.get("race-select")}, against : {$in : Session.get("against-select")}}, { sort: { createdAt: -1 } });
 	}
 });
+
+Template.buildsList.events({
+	"change #race-select" : function(e){
+		Session.set('race-select', [$(e.target).val().toLowerCase()])
+	},
+	"change #against-select" : function(e) {
+		Session.set('against-select', [$(e.target).val().toLowerCase()])
+	}
+})
