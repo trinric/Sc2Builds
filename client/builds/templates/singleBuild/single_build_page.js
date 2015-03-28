99e4710b7c677d5c7294fc493cba68f95e1f4264
Ryan Wilson <trinric@gmail.com>
2015-03-28 15:43:52 -0400

    Added an update page and verification for that

--- client/builds/templates/singleBuild/single_build_page.js
+++ client/builds/templates/singleBuild/single_build_page.js
@@ -2,6 +2,16 @@
 *
 *
 */
+
+Template.singleBuildPage.helpers({
+	isOwner : function() {
+		if(Meteor.user()._id == this.userId){
+			return true
+		}else {
+			return false
+		}
+	}
+});
 function nl2br (str, is_xhtml) {   
     var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';    
     return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
